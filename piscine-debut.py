d4a7f8645902a391df5394b82bb2d366583d650e
david.livrieri <david.livrieri974@gmail.com>
2024-11-25 10:58:57 +0100

    gg

--- piscine-debut.py
+++ piscine-debut.py
@@ -1,24 +1,36 @@
 liste = [("Pierre","Dos",10),("Paul","Brasse",13),("Léa","Crawl",6), ("Léa","Brasse",8) ]
 commande = ''
 
+def cmd_ajout(liste):
+    a = input("Qui nage ? ")
+    b = input("quelle nage ? ")
+    c = input("combien de longueur ? ")
+    liste.append((a,b,c))
+
+def cmd_liste(liste):
+    for elt in liste:
+        print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+
+def cmd_exit():
+    tmp == "exit"
+    isAlive = cmd_exit()
+    
 isAlive = True
+
 while isAlive:
     commande = input("Que faut-il faire ? ")
 
     if commande == 'ajout':
-        a = input("Qui nage ? ")
-        b = input("quelle nage ? ")
-        c = input("combien de longueur ? ")
-        liste.append((a,b,c))
+        cmd_ajout(liste)
    
     if commande == 'liste':
-        for elt in liste:
-            print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+        cmd_liste(liste)
+        
 
     if commande =='exit':
         tmp = input ("En etes vous sur ? oui/non")
         if tmp == "o":
-            isAlive = False
+            isAlive = cmd_exit()
         continue
 
     print (f"Commande {commande} inconnue")
